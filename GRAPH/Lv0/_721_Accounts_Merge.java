cc1e04f13b845bedad4ce557c09db767210f1bb7
souvik757 <88132246+souvik757@users.noreply.github.com>
2023-05-19 10:46:07 +0530

    <-- Union Find ✔ -->

--- GRAPH/Lv0/_721_Accounts_Merge.java
+++ GRAPH/Lv0/_721_Accounts_Merge.java
@@ -2,7 +2,61 @@ import java.util.* ;
 public class _721_Accounts_Merge {
     public static void main(String[] args) {
         //  run methods from object .....
+        SOLVE721 object = new SOLVE721() ;
+        List<List<String>> accounts = new ArrayList<>() ;
+        int N = 0 ;
+        String inputString = "" ;
+//  *  ["John","johnsmith@mail.com","john_newyork@mail.com"],
+//  *  ["John","johnsmith@mail.com","john00@mail.com"],
+//  *  ["Mary","mary@mail.com"],
+//  *  ["John","johnnybravo@mail.com"]
+//  *  -- Input Style --
+//  *  // * -- start
+//  *  size : 3
+//  *  John
+//  *  johnsmith@mail.com
+//  *  john_newyork@mail.com
+//  *  // * -- start
+//  *  size : 3
+//  *  John
+//  *  johnsmith@mail.com
+//  *  john00@mail.com
+//  *  // * -- start
+//  *  size : 2
+//  *  Mary
+//  *  mary@mail.com
+//  *  // * -- start
+//  *  size : 2
+//  *  John
+//  *  johnnybravo@mail.com
+//  *  // * -- ok
+        while (!inputString.equals("ok")){
+            System.out.print("// * -- ");
+            inputString = new Scanner(System.in).nextLine() ;
+            switch (inputString){
+                case "stop" :
 
+                    break ;
+                case "start" :
+                    List<String> input = new ArrayList<>() ;
+                    String inputs;
+                    System.out.print("size : ");
+                    N = new Scanner(System.in).nextInt() ;
+                    inputs = new Scanner(System.in).nextLine() ;
+                    input.add(inputs) ;
+                    int n = N-1 ;
+                    while (n-->0){
+                        inputs = new Scanner(System.in).nextLine() ;
+                        input.add(inputs) ;
+                    }
+                    accounts.add(input) ;
+                    break ;
+
+            }
+        }
+        accounts = object.accountsMerge(accounts) ;
+        System.out.println("// * -- Compressed accounts --");
+        for (List<String> account : accounts) System.out.println(account);
     }
 }
 class SOLVE721 {
@@ -53,8 +107,8 @@ class UnionFind {
         return root[x] ;
     }
     public void union(int x , int y){ // O(n)
-        int rootX = find(x) ;
-        int rootY = find(y) ;
+        int rootX = root[x] ;
+        int rootY = root[y] ;
         if(rootX != rootY) {
             for (int i = 0 ; i < root.length ; i ++) {
                 if (root[i] == rootY)
@@ -63,6 +117,6 @@ class UnionFind {
         }
     }
     public boolean connected(int x , int y){ // O(1)
-        return find(x) == find(y) ;
+        return root[x] == root[y] ;
     }
 }
---
+++ GRAPH/Lv0/_990_SatisfiabilityOfEqn.java
@@ -0,0 +1,54 @@
+import java.util.Map ;
+import java.util.HashMap ;
+public class _990_SatisfiabilityOfEqn {
+    public static void main(String[] args) {
+        SOLVE990 object = new SOLVE990() ;
+        System.out.println(object.equationsPossible(new String[]{"a==b","b!=a"})) ;
+        System.out.println(object.equationsPossible(new String[]{"a==b","b==c","b!=d","a==d"})) ;
+    }
+}
+class SOLVE990 {
+    public boolean equationsPossible(String[] equations){
+        DisjointSet set = new DisjointSet() ;
+        for (int i = 0 ; i < equations.length ; i ++){ // union each '==' characters
+            char c1 = equations[i].charAt(0) ;
+            char c2 = equations[i].charAt(1) ;
+            char c3 = equations[i].charAt(2) ;
+            char c4 = equations[i].charAt(3) ;
+            if (c2 == '=' && c3 == '=')
+                set.getUnion(c1 , c4) ;
+        }
+        for (int i = 0 ; i < equations.length ; i ++){ // any '!=' characters should not be connected directly or indirectly
+            char c1 = equations[i].charAt(0) ;
+            char c2 = equations[i].charAt(1) ;
+            char c3 = equations[i].charAt(2) ;
+            char c4 = equations[i].charAt(3) ;
+            if (c2 == '!' && c3 == '=') {
+                if(set.isConnected(c1, c4))
+                    return false ;
+            }
+        }
+        return true ;
+    }
+}
+class DisjointSet { // -- character sets --
+    private Map<Character,Character> roots ;
+    public DisjointSet() {
+        roots = new HashMap<>() ;
+        for (int i = 0 ; i <= 26 ; i ++)
+            roots.put((char) (i+96) , (char) (i+96)) ;
+    }
+    public void getUnion(char a,char b){
+        char root_a = roots.get(a) ;
+        char root_b = roots.get(b) ;
+        if (root_a != root_b) {
+            for (int i = 0 ; i < roots.size() ; i ++){
+                if(roots.get((char)(i+96)) == root_b)
+                    roots.put((char)(i+96) , root_a) ;
+            }
+        }
+    }
+    public boolean isConnected(char a,char b){
+        return roots.get(a) == roots.get(b) ;
+    }
+}

bb8b06bcc3f29b6aeaaa914e4a06aac125e79db7
souvik757 <88132246+souvik757@users.noreply.github.com>
2023-05-24 09:05:04 +0530

    <! -- Linked List from Interview 150 -->✔

---
+++ LINKED  LIST/ LV1 /_82_RemoveDuplicateFromSorted_II.java
@@ -0,0 +1,85 @@
+import java.util.Map ;
+import java.util.HashMap ;
+import java.util.Scanner;
+
+public class _82_RemoveDuplicateFromSorted_II {
+    public static void main(String[] args) {
+        SOLVE82 object = new SOLVE82() ;
+        SOLVE82Constant obj = new SOLVE82Constant() ;
+        ListNode head = new ListNode() ;
+        head = head.GetDemoList(head , 5);
+        head.show(head);
+        System.out.println();
+        int val = 0;
+        while (val != -1){
+            val = new Scanner(System.in).nextInt() ;
+            if(val != -1)
+                head = head.insert_head(head , val);
+        }
+        head.show(head);
+//        head = object.deleteDuplicates(head) ;
+        head = obj.deleteDuplicates(head) ;
+        System.out.println();
+        head.show(head);
+    }
+}
+class SOLVE82{ // native approach using HashMap<>() :::
+    Map<Integer, Integer> frequency = new HashMap<>() ;
+    public ListNode deleteDuplicates(ListNode head) {
+        UpdateMap(head) ;
+        for(Map.Entry<Integer,Integer> entry : frequency.entrySet()){
+            if(entry.getValue() > 1) {
+//                System.out.println(entry.getKey()+" has multi occ");
+                head = DeleteAllOcc(head, entry.getKey());
+            }
+        }
+//        System.gc() ;
+        return head ;
+    }
+    private void UpdateMap(ListNode head){
+        ListNode current = head ;
+        while (current != null){
+            frequency.put(current.val , frequency.getOrDefault(current.val , 0)+1) ;
+            current = current.next ;
+        }
+//        System.out.println(frequency);
+    }
+    private ListNode DeleteAllOcc(ListNode head , int val){
+        ListNode current = head ;
+        if(head == null)
+            return null;
+        while (current != null && current.val == val){
+            current = current.next;
+            head = current ;
+        }
+        ListNode prev = new ListNode(-1) ; // following node after current :::
+        while (current != null){
+            if(current.val == val)
+                prev.next = current.next ;
+            else
+                prev = current ;
+            current = current.next ;
+        }
+
+        return head ;
+    }
+}
+class SOLVE82Constant{
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null) return null;
+        ListNode prev = new ListNode(-1) ; // current|----> head|---->
+        ListNode current = prev ;
+        current.next = head ;
+        while (current.next != null && current.next.next != null){
+            if(current.next.val == current.next.next.val){ // head != null && head.next != null
+                int duplicate = current.next.val ; // store the duplicate value to traverse until found unique :::
+                while (current.next != null && current.next.val == duplicate){ // keep skipping duplicate :::
+                    current.next = current.next.next ;
+                }
+            }
+            else
+                current = current.next ;
+        }
+        return prev.next ;
+    }
+}

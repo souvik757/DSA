0cdd5a6761b617cbc96ae015eede808b5a6e66e6
souvik757 <88132246+souvik757@users.noreply.github.com>
2023-05-27 08:26:53 +0530

    Arrays. .  ✔

--- SLIDING_WINDOW/_80_RemoveDuplicates_II.java
+++ SLIDING_WINDOW/_80_RemoveDuplicates_II.java
@@ -3,9 +3,12 @@ public class _80_RemoveDuplicates_II {
     public static void main(String[] args) {
         SOLVE80 object = new SOLVE80() ;
         System.out.println(object.removeDuplicates(new int[]{1,1,1,2,2,3}));
+        System.out.println(object.removeDuplicatesOptimized(new int[]{1,1,1,2,2,3}));
+        System.out.println(object.RemoveDuplicates(new int[]{1,1,1,2,2,3}));
     }
 }
 class SOLVE80{
+    // * -- Map solution -- approach 1
     public int removeDuplicates(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int val : nums) {
@@ -30,18 +33,35 @@ class SOLVE80{
         System.out.println(Arrays.toString(nums));
         return total;
     }
-// *   public int removeDuplicates(int[] nums) {
-// *       Map<Integer, Integer> map = new HashMap<>();
-// *       List<Integer> result = new ArrayList<>();
-// *       for (int val : nums) {
-// *           map.put(val, map.getOrDefault(val, 0) + 1);
-// *           if (map.get(val) <= 2) {
-// *               result.add(val);
-// *           }
-// *       }
-// *       for (int i = 0; i < result.size(); i++) {
-// *           nums[i] = result.get(i);
-// *       }
-// *       return result.size();
-// *   }
+    public int removeDuplicatesOptimized(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+        for (int val : nums) {
+            map.put(val, map.getOrDefault(val, 0) + 1);
+            if (map.get(val) <= 2) {
+                result.add(val);
+            }
+        }
+        for (int i = 0; i < result.size(); i++) {
+            nums[i] = result.get(i);
+        }
+        return result.size();
+    }
+    // * -- optimized solution O(n) & constant space --
+    public int RemoveDuplicates(int[] nums){
+        int count = 0 ;
+        int index = 1 ;
+        for (int i = 1 ; i < nums.length ; i ++){
+            if(nums[i] == nums[i-1])
+                count++ ;
+            else
+                count = 0 ;
+            if(count <= 1) {
+                nums[index] = nums[i];
+                index++ ;
+            }
+        }
+
+        return index ;
+    }
 }

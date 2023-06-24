421dcf2e8f119776eae128fe862c851e0e025a5c
souvik757 <88132246+souvik757@users.noreply.github.com>
2023-06-24 09:06:57 +0530

    string permutation . . .  ✔

--- SLIDING_WINDOW/permutation_of_string.java
+++ SLIDING_WINDOW/permutation_of_string.java
@@ -2,60 +2,56 @@ import java.util.*;
 
 public class permutation_of_string
 {
-    // FOR INTEGER :
-    static void solve(List<List<Integer>> answer , List<Integer> output , int[] number)
-    {
-        // BASE CASE :
-        if(output.size() == number.length)
-        {
-            answer.add(new ArrayList<>(output)) ;
-        }
-        else
-        {
-            for (int i : number) {
-                if (output.contains(i))
-                    continue;
-                output.add(i);
-                solve(answer, output, number);
-                output.remove(output.size() - 1);
-            }
-        }
-    }
-
-    // FOR STRING :
-    static void solve(List<List<String>> answer , List<String > output , String[] number)
-    {
-        // BASE CASE :
-        if(output.size() == number.length)
-        {
-            answer.add(new ArrayList<>(output)) ;
-        }
-        else
-        {
-            for (String i : number) {
-                if (output.contains(i))
-                    continue;
-                output.add(i);
-                solve(answer, output, number);
-                output.remove(output.size() - 1);
-            }
-        }
-    }
     public static void main(String[] args)
     {
-        // INTEGER :
-        List<List<Integer>> answer = new ArrayList<>() ;
-        List<Integer> output = new ArrayList<>() ;
-        int[] numbers  = new int[]{1,2,3} ;
-        solve(answer , output , numbers ) ;
-        System.out.println("Permutation of "+ Arrays.toString(numbers)+" : "+answer);
-
-        // STRING :
-        List<List<String>> answer_str = new ArrayList<>() ;
+        // string :
+        _46_String obj2 = new _46_String() ;
         List<String> output_str = new ArrayList<>() ;
         String[] str  = new String[]{"s" , "o" , "u"} ;
-        solve(answer_str , output_str , str ) ;
-        System.out.println("Permutation of "+ Arrays.toString(str)+" : "+answer_str);
-
+        List<List<String>> answer_str = new ArrayList<>() ;
+        // using extra space
+        obj2.solve(answer_str , output_str , str ) ;
+        System.out.println("Permutation of "+Arrays.toString(str)+"\n : "+answer_str);
+        answer_str.clear() ;
+        // without using extra space
+        obj2._solve_(answer_str , str , 0) ;
+        System.out.println("Permutation of "+Arrays.toString(str)+"\n -> "+answer_str);
+    }
+}
+class _46_String{
+    // Using extra space
+    public void solve(List<List<String>> answer , List<String > output , String[] str) {
+        if(output.size() == str.length)
+            answer.add(new ArrayList<>(output)) ;
+        else {
+            for (String val : str) {
+                if (output.contains(val)) continue ;
+                output.add(val);
+                solve(answer,output,str);
+                output.remove(output.size()-1);
+            }
+        }
+    }
+    // Without using space
+    public void _solve_(List<List<String>> ans , String[] str , int index) {
+        if (index == str.length) {
+            copy(ans,str);
+            return ;
+        }
+        for (int i = index ; i < str.length ; i ++) {
+            String temp1 = str[index] ;
+            str[index] = str[i] ;
+            str[i] = temp1 ;
+            _solve_(ans,str,index+1);
+            String temp2 = str[index] ;
+            str[index] = str[i] ;
+            str[i] = temp2 ;
+        }
+    }
+    private void copy(List<List<String>> ans,String[] str) {
+        List<String> list = new ArrayList<>() ;
+        for (String val : str)
+            list.add(val) ;
+        ans.add(new ArrayList<>(list)) ;
     }
 }

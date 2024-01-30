9cf7c4ae5974e2b544dc48d348d2fbda740b0fe2
Souvik Maity <souvik.gta@gmail.com>
2024-01-30 10:17:04 +0530

    <-- [word search] -->

--- GRAPH/Lv0/_212_Word_Search_II.java
+++ GRAPH/Lv0/_212_Word_Search_II.java
@@ -18,17 +18,64 @@ public class _212_Word_Search_II {
         for (char[] ch : grid){
             System.out.println(Arrays.toString(ch));
         }
-        bruteForce_solve_212 obj = new bruteForce_solve_212() ;
-        System.out.println(obj.findWords(grid,words));
+//        bruteForce_solve_212 obj = new bruteForce_solve_212() ;
+//        System.out.println(obj.findWords(grid,words));
+        solve_212 obj1 = new solve_212() ;
+        System.out.println(obj1.findWords(grid,words));
     }
 }
-//class solve_212 {
-//    public List<String> findWords(char[][] board, String[] words) {
-//        List<String> answers = new ArrayList<>() ;
-//
-//        return answers ;
-//    }
-//}
+class solve_212 {
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> answers = new ArrayList<>() ;
+        TrieNode root = buildTrie(words) ;
+        for (int i = 0 ; i < board.length ; i ++){
+            for (int j = 0 ; j < board[0].length ; j ++)
+                dfs(i,j,board,root,answers) ;
+        }
+        return answers ;
+    }
+
+    private void dfs(int i, int j, char[][] board, TrieNode root, List<String> answers) {
+        char c = board[i][j] ;
+        if(c == '#' || root.next[c-'a'] == null)
+            return;
+        root = root.next[c-'a'] ;
+        if(root.word != null){
+            answers.add(root.word) ;
+            root.word = null ;
+        }
+
+        board[i][j] = '#' ;
+        if(i > 0)
+            dfs(i-1,j,board,root,answers);
+        if(j > 0)
+            dfs(i,j-1,board,root,answers);
+        if(i < board.length - 1)
+            dfs(i+1,j,board,root,answers);
+        if(j < board[0].length - 1)
+            dfs(i,j+1,board,root,answers);
+        board[i][j] = c ;
+    }
+
+    private TrieNode buildTrie(String[] words){
+        TrieNode root = new TrieNode() ;
+        for (int i = 0 ; i < words.length ; i ++){
+            TrieNode p = root ;
+            for (int j = 0 ; j < words[i].toCharArray().length ; j ++){
+                int k = words[i].toCharArray()[j] - 'a' ;
+                if(p.next[k] == null)
+                    p.next[k] = new TrieNode() ;
+                p = p.next[k] ;
+            }
+            p.word = words[i] ;
+        }
+        return root ;
+    }
+    private class TrieNode {
+        String word ;
+        TrieNode[] next = new TrieNode[26] ;
+    }
+}
 class bruteForce_solve_212 {
     public List<String> findWords(char[][] board, String[] words) {
         List<String> answers = new ArrayList<>() ;
